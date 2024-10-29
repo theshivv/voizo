cbd0a40d4e5a2bf648d4f26c2c2f3440ce427ffc
theshivv <shivamdarasingpawar@gmail.com>
2024-10-29 15:41:22 +0530

    removed unwanted lucide react import

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -8,7 +8,7 @@ import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { ImageIcon, VideoIcon, AudioIcon } from "lucide-react"
+
 
 type Post = {
   id: number
